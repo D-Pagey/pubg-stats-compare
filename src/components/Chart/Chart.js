d3c1ca284a8e8813c570a7fd41ae3e4a47566956
D-Pagey <dan.page91@gmail.com>
2018-06-10 04:34:08 +0100

    adding xAxis and labels.

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { scaleLinear } from 'd3-scale';
 import { max } from 'd3-array';
 import { select } from 'd3-selection';
+import { axisBottom, axisLeft } from 'd3-axis';
 import './Chart.css';
 
 export default class Chart extends Component {
@@ -17,9 +18,14 @@ export default class Chart extends Component {
     }
 
     createChart = () => {
+
         const xScale = scaleLinear()
-            .domain([0, max(this.props.data)])
-            .range([0, this.props.size[0]])
+        .domain([0, max(this.props.data)])
+        .range([0, this.props.size[0] - 50])
+
+        const xAxis = axisBottom(xScale);
+
+
 
         select(this.node)
             .selectAll('rect')
@@ -36,16 +42,30 @@ export default class Chart extends Component {
         select(this.node)
             .selectAll('rect')
             .data(this.props.data)
-            .style('fill', 'green')
-            .attr('x', d => xScale(d)- this.props.size[1])
-            .attr('y', (d,i) => i * 100)
+            .attr('x', 0)    
+            .attr('y', (d, i) => i * 60)
             .attr('width', d => xScale(d))
             .attr('height', 50)
+            .style('fill', 'green')
+
+        select(this.node)
+            .selectAll('text')
+            .data(this.props.data)
+            .enter()
+            .append('text')
+            .text((d) => d + ' k/d')
+            .attr('x', (d, i) => xScale(d) + 5)
+            .attr('y', (d, i) => i * 60 + 30)
+
+        select(this.node)
+            .append('g')
+            .attr('transform', 'translate(0, ' + 200 + ')')
+            .call(xAxis)
    }
     
    render() {
       return <svg ref={node => this.node = node} className='svg'
-      width={500} height={500}>
+      width={550} height={250}>
       </svg>
    }
 }
