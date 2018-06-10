5249176c463eb739a2b0ec4c6f975dad056a1c65
Nav Garcha <nav.garcha@clearscore.com>
2018-06-10 04:38:04 +0100

    fix pubg api calls

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
 import React, { Component } from 'react';
-import API from './api';
+import { getLatestMatch } from './api';
 import './App.css';
 import Chart from './components/Chart';
 
 export default class App extends Component {
-	state = {}
-	api = new API()
-	
-	async componentDidMount() {
-		this.setState({ data: await this.api.getMatchLatestMatch('r4g3d') });
+	state = {
+		playerName: 'r4g3d',
+		chartData: null,
+	}
+
+	componentDidMount() {
+		this.requestChartData();
+	}
+
+	async requestChartData() {
+		const { playerName } = this.state;
+		const chartData = await getLatestMatch(playerName);
+		
+		this.setState({ chartData });
 	}
 
 	render() {
-		const { data } = this.state;
+		const { chartData } = this.state;
 
 		return (
 			<div>
 				<h1>PUBG Stats Compare</h1>
-				<pre>{JSON.stringify(data, null, 4)}</pre>
+				<pre>{JSON.stringify(chartData, null, 4)}</pre>
+
 				<Chart 
-				data={[0.90, 1.1, 1.5]} 
-				size={[500, 500]}
-				padding={30} />
+					data={[0.90, 1.1, 1.5]} 
+					size={[500, 500]}
+					padding={30} 
+				/>
 			</div>
 		);
 	}
--- src/api.js
+++ src/api.js
@@ -1,39 +1,11 @@
-import axios from 'axios';
+import pubg from 'pubg.js';
 
 const API_TOKEN = process.env.REACT_APP_API_TOKEN;
-const API_URL = 'https://api.playbattlegrounds.com/shards/';
+const API_PLATFORM = 'pc-eu';
 
-export default class API {
-    constructor(apiPlatform = 'pc-eu') {
-        if (!API_TOKEN) throw new Error('No `REACT_APP_API_TOKEN` supplied.');
-        
-        this.request = axios.create({
-            baseURL: `${API_URL}${apiPlatform}`,
-            headers: {
-                Accept: 'application/vnd.api+json',
-                Authorization: `Bearer ${API_TOKEN}`
-            }
-        });
-        
-    }
+const client = new pubg.Client(API_TOKEN, API_PLATFORM);
 
-    async makeRequest(uri) {
-        return await this.request.get(uri)
-            .then((reponse) => reponse.data.data)
-            .catch((error) => console.error(error));
-    }
-
-    getPlayer(playerName) {
-        return this.makeRequest(`players?filter[playerNames]=${playerName}`)
-            .then((data) => data[0]);
-    }
-
-    getMatch(matchId) {
-        return this.makeRequest(`matches/${matchId}`);
-    }
-
-    async getMatchLatestMatch(playerName) {
-        const { relationships } = await this.getPlayer(playerName);
-        return this.getMatch(relationships.matches.data[0].id);
-    }
-}
+export const getLatestMatch = async (playerName) => {
+    const player = await client.getPlayer({ name: playerName });
+    return await client.getMatch(player.relationships.matches[0].id);
+};
