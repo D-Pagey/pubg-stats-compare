774d3ea2155e14a2ac61eaef1d9f70cdd06170f7
Pagey <dan.page91@gmail.com>
2018-06-12 14:55:33 +0100

    chart real data

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -18,7 +18,7 @@ export default class App extends Component {
 	async fetchChartData() {
 		const { playerName } = this.state;
 		const match = await this.api.fetchLatestMatch(playerName);
-		const teamDamagePercent = new Stats(match).getTeamDamagePercent(playerName);
+		const teamDamagePercent = new Stats(match).getTeamKdr(playerName);
 
 		this.setState({ chartData: teamDamagePercent });
 	}
@@ -26,15 +26,17 @@ export default class App extends Component {
 	render() {
 		const { chartData } = this.state;
 
+		if (!chartData) return null;
+
 		return (
 			<div>
 				<h1>PUBG Stats Compare</h1>
 				<pre>{JSON.stringify(chartData, null, 4)}</pre>
 
-				<Chart 
-					data={[0.90, 1.1, 1.5]} 
+				<Chart
+					data={chartData.map(({ kdr }) => kdr)}
 					size={[500, 500]}
-					padding={30} 
+					padding={30}
 				/>
 			</div>
 		);
